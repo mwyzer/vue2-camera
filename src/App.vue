e3e2f37a6a04dd102fc49924fb0d7162c1111073
mwyzer <muhammad.wyzer@gmail.com>
2021-12-23 20:59:07 +0700

    finish

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <camera />
+  </div>
+</template>
+
+<script>
+import Camera from "./components/Camera.vue";
+export default {
+  components: {
+    Camera,
+  },
+};
+</script>
+
+
+<style>
+</style>
